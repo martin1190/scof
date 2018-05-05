24bf2d166d293d001ed8e5ec89480c88adbf3855
martin1597 <antunezmartin@outlook.com.pe>
2018-05-04 22:11:45 -0500

    V 1.04.2

--- resources/views/vendor/adminlte/pages/reportes/diagnostico/generarreportediagnostico.blade.php
+++ resources/views/vendor/adminlte/pages/reportes/diagnostico/generarreportediagnostico.blade.php
@@ -15,26 +15,101 @@
 @section('main-content')
   @if(Auth::user()->tipo_users_id==1 || Auth::user()->tipo_users_id==2)
     <div class="row">
-        <div class="panel panel-info">
-          <div class="panel-heading">
-            Reporte por diagnostico
-          </div>
-          <div class="panel-body">
-            <form  id="frmDiario">
-              <div class="form-group">
-                <label class="col-sm-1">Desde:</label>
-                <div class="col-sm-2">
-                  <input type="date" class="form-control input-sm" id="fecIniD">
-                </div>
-                <label class="control-label col-sm-1">Hasta:</label>
-                <div class="col-sm-2">
-                  <input type="date" class="form-control input-sm" id="fecFinD">
-                </div>
-                <a class="btn btn-success btn-sm" id="btnBusRD">Generar</a>
+        <div class="col-md-12">
+          <div class="box box-warning collapsed-box box-solid">
+            <div class="box-header with-border">
+              <h3 class="box-title">Resumen general de diagnostico por fechas</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                </button>
               </div>
-            </form>
+              <!-- /.box-tools -->
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">                  
+              <form  id="frmDiario">
+                <div class="form-group">
+                  <label class="col-sm-1">Desde:</label>
+                  <div class="col-sm-2">
+                    <input type="date" class="form-control input-sm" id="fecIniD">
+                  </div>
+                  <label class="control-label col-sm-1">Hasta:</label>
+                  <div class="col-sm-2">
+                    <input type="date" class="form-control input-sm" id="fecFinD">
+                  </div>
+                  <a class="btn btn-success btn-sm" id="btnBusRD">Generar</a>
+                </div>
+              </form>                  
+            </div>
+            <!-- /.box-body -->
           </div>
-        </div>
+          <!-- /.box -->
+        </div>      
+        <!-- Expandible resumen-->
+        <div class="col-md-12">
+          <div class="box box-info collapsed-box box-solid">
+            <div class="box-header with-border">
+              <h3 class="box-title">Resumen de diagnosticos por fechas</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                </button>
+              </div>
+              <!-- /.box-tools -->
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">                  
+              <form  id="frmDiario">
+                <div class="form-group">
+                  <label class="col-sm-1">Desde:</label>
+                  <div class="col-sm-2">
+                    <input type="date" class="form-control input-sm" id="fecIniD">
+                  </div>
+                  <label class="control-label col-sm-1">Hasta:</label>
+                  <div class="col-sm-2">
+                    <input type="date" class="form-control input-sm" id="fecFinD">
+                  </div>
+                  <a class="btn btn-success btn-sm" id="btnBusRD">Generar</a>
+                </div>
+              </form>                  
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>  
+        <!-- Expandible mas frecuente-->        
+        <div class="col-md-12">
+          <div class="box box-default collapsed-box box-solid">
+            <div class="box-header with-border">
+              <h3 class="box-title">Resumen de diagnosticos por fechas</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                </button>
+              </div>
+              <!-- /.box-tools -->
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">                  
+              <form  id="frmDiario">
+                <div class="form-group">
+                  <label class="col-sm-1">Desde:</label>
+                  <div class="col-sm-2">
+                    <input type="date" class="form-control input-sm" id="fecIniD">
+                  </div>
+                  <label class="control-label col-sm-1">Hasta:</label>
+                  <div class="col-sm-2">
+                    <input type="date" class="form-control input-sm" id="fecFinD">
+                  </div>
+                  <a class="btn btn-success btn-sm" id="btnBusRD">Generar</a>
+                </div>
+              </form>                  
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>          
       </div>
       <div class="row">
         <a href="#" class="btn btn-info" id="printRD"><i class="fa fa-print">Imprimir</i></a>
