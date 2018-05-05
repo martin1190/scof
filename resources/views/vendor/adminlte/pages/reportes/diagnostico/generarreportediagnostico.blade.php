364aa23cb1efb95573636bb7afaeda38bfd542f6
martin1597 <antunezmartin@outlook.com.pe>
2018-05-04 22:45:04 -0500

    V 1.04.3

--- resources/views/vendor/adminlte/pages/reportes/diagnostico/generarreportediagnostico.blade.php
+++ resources/views/vendor/adminlte/pages/reportes/diagnostico/generarreportediagnostico.blade.php
@@ -82,7 +82,7 @@
         <div class="col-md-12">
           <div class="box box-default collapsed-box box-solid">
             <div class="box-header with-border">
-              <h3 class="box-title">Resumen de diagnosticos por fechas</h3>
+              <h3 class="box-title">Resumen de dignosticos mas frecuentes</h3>
 
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
